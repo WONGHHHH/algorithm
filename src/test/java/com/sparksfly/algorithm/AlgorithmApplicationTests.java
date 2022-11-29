b53287acc0a3df30f928d26e089d4e93f85f2007
wanghe <wangh@ckt.cn>
2022-11-29 14:08:15 +0800

    Initial commit

---
+++ src/test/java/com/sparksfly/algorithm/AlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparksfly.algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
